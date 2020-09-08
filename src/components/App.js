c4e7c946242816574cedbbe36efdacfe3038ee47
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2020-09-08 16:06:47 +0300

    added transition

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 
 import PhoneBook from './PhoneBook';
 import ContactProperties from './ContactProperties';
-import { Route, Switch } from 'react-router-dom';
 
+import './app.scss'
 
 function App() {
   return (
-    <div className='App'>
-      <Switch>
-        <Route exact path='/phoneBook' component={PhoneBook} />
-        <Route exact path="/items/:itemId" component={ContactProperties} />
-      </Switch>
-    </div>
+    <Route render={({ location }) => (
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={450}
+          classNames="fade"
+        >
+          <Switch location={location}>
+            <Route exact path='/phoneBook' component={PhoneBook} />
+            <Route exact path="/items/:itemId" component={ContactProperties} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    )} />
   );
-};
+}
 
 export default App;
